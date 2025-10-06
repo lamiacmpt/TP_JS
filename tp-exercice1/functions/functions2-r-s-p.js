42097c91f1535d3f80c83dddaf58bff0b623345d
Lamyae <lamia@gmail.com>
2025-10-05 23:39:22 -0700

    fuction autoplayer

--- tp-exercice1/functions/functions2-r-s-p.js
+++ tp-exercice1/functions/functions2-r-s-p.js
@@ -23,57 +23,52 @@ document.querySelector('.js-scissors-button')
     playGame('scissors');
   });
 
-
-
-document.addEventListener('keydown', (event) => {
+  document.addEventListener('keydown',(event) => {
   if (event.key === 'r' || event.key === 'R') {
     playGame('rock');
   } else if (event.key === 'p' || event.key === 'P') {
     playGame('paper');
   } else if (event.key === 's' || event.key === 'S') {
     playGame('scissors');
-  }
-});
+  }});
 
 
 
 function playGame(playerMove) {
-
   const computerMove = pickComputerMove();
+
   let result = '';
 
- // calculate result
+  // calculate result
 
-  if (playerMove === computerMove) {
-    result = 'Tie.';
+  if (playerMove === computerMove){
+    result = 'Tie';
     score.ties++;
-  } else if (
-    (playerMove === 'rock' && computerMove === 'scissors') ||
-    (playerMove === 'paper' && computerMove === 'rock') ||
-    (playerMove === 'scissors' && computerMove === 'paper')
-  ) {
-    result = 'You win';
+  }
+  else if (
+  (playerMove === 'rock' && computerMove === 'scissors')||
+  (playerMove === 'paper' && computerMove === 'rock')||
+  (playerMove === 'scissors' && computerMove === 'paper')
+  ){
+    result = 'you win';
     score.wins++;
   } else {
-    result = 'You lose.';
+    result = 'you lose';
     score.losses++;
+
   }
+
   // update the score and store it using localStorage.setItem
-
-  localStorage.setItem('score', JSON.parse(score));
-
-  
-  // show the new score and the updated images using "document.querySelector"
-
-  updateScoreElement();
-  
+  localStorage.setItem('score',JSON.stringify(score));
+  // show the new score 
+  updateScoreElement()
+  // and the updated images using "document.querySelector"
 
   document.querySelector('.js-result').innerHTML = result;
   document.querySelector('.js-moves').innerHTML = `
     <img src="images/${playerMove}-emoji.png" class="move-icon">
     <img src="images/${computerMove}-emoji.png" class="move-icon">
   `;
- 
 
 }
 
@@ -96,4 +91,25 @@ function pickComputerMove() {
   }
 
   return computerMove;
+}
+ let isAutoPlaying = false;
+let intervalId; 
+
+function toggleAutoPlay() {
+  const button = document.querySelector('.js-auto');
+
+  if (!isAutoPlaying) {
+    button.innerText = 'Stop Auto Play';
+    isAutoPlaying = true;
+
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+
+  } else {
+    button.innerText = 'Auto Play';
+    isAutoPlaying = false;
+    clearInterval(intervalId);
+  }
 }
